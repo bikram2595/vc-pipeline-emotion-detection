12c810839049555115f24f4a54bae0d173d59356
Bikram Das <bikramkumardas2512@gmail.com>
2025-02-03 09:18:48 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='emotion detection project with VC and ML pipeline',
+    author='bikram',
+    license='',
+)
